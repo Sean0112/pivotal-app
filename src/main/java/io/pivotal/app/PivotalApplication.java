1f2fe08b32acc740b2bbc2c06ed51e9e93987b26
jfernandez <jfernandez@gblsys.com>
2019-04-25 19:22:22 -0700

    Initial project build

---
+++ src/main/java/io/pivotal/app/PivotalApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PivotalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PivotalApplication.class, args);
+    }
+}
